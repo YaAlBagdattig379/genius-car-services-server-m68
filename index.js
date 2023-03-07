185b6f4ab0fc4ab7eb6f0a4b511ed084a6f01e5d
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-07 13:00:49 +0530

    v68 v7

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+            if(error){
+              return res.status(403).send({message:'Forbidden access'});
+            }
+            req.decoded = decoded;
+            console.log('decoded',decoded);
+            next(); 
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xw2abv6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -59,7 +76,9 @@ async function run() {
         })
 
         // order collection API 
-        app.get('/order',async(req,res)=>{
+        app.get('/order',verifyJWT,async(req,res)=>{
+           const authHeader = req.headers.authorization;
+          //  console.log(authHeader) 
            const email = req.query.email;
           //  const query = {};
            const query = {email: email};
